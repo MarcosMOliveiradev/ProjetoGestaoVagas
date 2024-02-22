ce67dc9f6f8c99901b569a244752510754e72a88
Marcos Monteiro <marcos.moliveira@outlook.com>
2024-02-22 11:45:15 -0300

    making some validate

--- src/main/java/br/com/marcos/gestao_vagas/modules/candidate/CandidateEntity.java
+++ src/main/java/br/com/marcos/gestao_vagas/modules/candidate/CandidateEntity.java
@@ -2,6 +2,10 @@ package br.com.marcos.gestao_vagas.modules.candidate;
 
 import java.util.UUID;
 
+import org.hibernate.validator.constraints.Length;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 @Data
@@ -9,8 +13,14 @@ public class CandidateEntity {
 
     private UUID id;
     private String name;
+
+    @Pattern(regexp = "^(?!\\s*$).+", message = "O campo [Username] não deve conter espaço")
     private String username;
+
+    @Email(message = "O campo [email] deve conter um Email valido!")
     private String email;
+
+    @Length(min = 6, max = 20)
     private String password;
     private String description;
     private String curriculum;
